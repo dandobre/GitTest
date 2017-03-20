5ee77a87d2730429add8366c58fb4fb3589f719f
Student <Student@zpr-l0007075.local>
2017-03-20 09:18:35 -0400

    Created button and label

--- GIT intro/ViewController.swift
+++ GIT intro/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
+    @IBOutlet weak var labelText: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func buttonPress(_ sender: Any) {
+        count += 1
+        if count == 1 {
+            labelText.text = "Pressed once"
+        }
+        else {
+            labelText.text = "Pressed " + String(count) + " times"
+        }
     }
 
 
